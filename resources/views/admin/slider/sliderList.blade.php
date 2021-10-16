2d851b26378bf9ea0a7f43c463ba9469e5adb52c
joelitok <joelnkouatchet@gmail.com>
2021-10-17 00:47:04 +0100

    add product

--- resources/views/admin/slider/sliderList.blade.php
+++ resources/views/admin/slider/sliderList.blade.php
@@ -31,7 +31,7 @@
   <tr>
   <td>{{$increment}}</td>
 {{--  <td><img src="/storage/slider_images/{{$slider->slider_image}}"></td>  --}}
-<td>  <img src="slider_images/{{ $slider->slider_image}}" ></td>
+<td>  <img src="{{URL::to('/')}}/slider_images/{{ $slider->slider_image}}" ></td>
                                                        
  <td>{{$slider->slider_name}}</td>
  <td>{{$slider->slider_price}}</td>
