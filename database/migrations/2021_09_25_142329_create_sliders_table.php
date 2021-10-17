81355b8196e4c8b918383016b5b9a3ca9eba926c
joelitok <joelnkouatchet@gmail.com>
2021-10-17 03:32:13 +0100

    remove secure

--- database/migrations/2021_09_25_142329_create_sliders_table.php
+++ database/migrations/2021_09_25_142329_create_sliders_table.php
@@ -18,7 +18,7 @@ class CreateSlidersTable extends Migration
             $table->string('slider_name');
             $table->integer('slider_price');
             $table->text('slider_description');
-            $table->string('slider_image');
+            $table->text('slider_image');
             $table->integer('slider_status');
             $table->timestamps();
         });
--- resources/views/admin/slider/sliderList.blade.php
+++ resources/views/admin/slider/sliderList.blade.php
@@ -31,7 +31,7 @@
   <tr>
   <td>{{$increment}}</td>
 {{--  <td><img src="/storage/slider_images/{{$slider->slider_image}}"></td>  --}}
-<td>  <img src="{{ $slider->slider_image}}" >
+<td>  <img src="{{$slider->slider_image}}" >
 
 </td>
                                                        
