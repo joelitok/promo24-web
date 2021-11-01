f4eef286918a24b4c8b07784fc439798f049445a
joelitok <joelnkouatchet@gmail.com>
2021-11-01 23:30:07 +0100

    addd

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -2,13 +2,13 @@
     <div class="container">
         <ol class="breadcrumb" style="color: black">
             <li class="breadcrumb-item"><a href="#"><i class="icon-home"></i></a></li>
-            <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.6rem">
+            <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.8rem">
              <a href="{{URL::to('/')}}"style="color: black;bold:2px" ><strong>Accueil</strong></a> </li>
-            <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.6rem">
+            <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.8rem">
                  <a href="{{URL::to('/city_c')}}"  style="color: black" ><strong>Ville</strong></a> </li>
-                <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.6rem">
+                <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.8rem">
                   <a href="{{URL::to('/category_c')}}" style="color: black"><strong>Catégories</strong></a></li>
-                    <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.6rem">
+                    <li class="breadcrumb-item active" aria-current="page" style="font-size: 1.8rem">
                          <a href="{{URL::to('/catalogue_c')}}"style="color: black"  ><strong>Catalogues</strong></a> </li>
         </ol>
     </div><!-- End .container -->
